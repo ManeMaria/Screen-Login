bb0a0656e258cf859316afd86a6c8a108a444705
ManeMaria <mirandacesar598@gmail.com>
2021-03-19 11:48:38 -0300

    novas atualizações

--- src/Styles/global.js
+++ src/Styles/global.js
@@ -9,30 +9,28 @@ export default createGlobalStyle`
     width: 100%;
     outline: none;
     box-sizing: border-box;
-    display: flex;
-    align-items: center;
-    justify-content: center;
     background: linear-gradient(-45deg,#48d1cc, #00ced1,#20b2aa, #008b8b) no-repeat;
     background-size: 300% 300%;
-    animation: gradient 5s ease infinite;
+
+    animation: gradient 7s ease infinite 2s;
    
     @keyframes gradient{
        0%{
-        background-position: 0% 50%;
+        background-position: 0% 25%;
        }
       
        25%{
-        background-position: 50% 100%;
+        background-position: 25% 50%;
        }
        50%{
-        background-position: 100% 0%;
+        background-position: 50% 75%;
        }
        
        75%{
-        background-position: 0% 50%;
+        background-position: 75% 100%;
     } 
        100%{
-        background-position:  50% 100%;
+        background-position:  100% 0%;
        }
     }
 
